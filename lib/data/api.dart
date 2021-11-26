c221e463f1a11089b98ce1ceed4f1467f0daa4a0
Anwar Kabir <munnarahman1994@gmail.com>
2021-11-26 17:19:22 +0600

    api added

---
+++ lib/data/api.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class API extends StatelessWidget {
+  const API({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
